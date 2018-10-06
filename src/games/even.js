58618d7333ef13b95ecac4dbab87d8e23358f507
raylyanway <raylyanway@gmail.com>
2018-10-06 11:39:05 +0300

    refactor(even.js) rename variables

--- src/games/even.js
+++ src/games/even.js
@@ -1,22 +1,22 @@
 import readlineSync from 'readline-sync';
 
 const isEven = number => number % 2 === 0;
+const attemptsCount = 3;
 
 export default () => {
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".');
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
-  const attemptsCount = 3;
   for (let i = 0; i < attemptsCount; i += 1) {
-    const randomNumber = Math.floor(Math.random() * 100);
-    const check = isEven(randomNumber) ? 'yes' : 'no';
-    console.log(`Question: ${randomNumber}`);
+    const question = Math.floor(Math.random() * 100);
+    const rightAnswer = isEven(question) ? 'yes' : 'no';
+    console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
-    if (answer === check) {
+    if (answer === rightAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${check}'.`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
