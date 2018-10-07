aafe337ac9c3acc9e37e53304fec29e4d51463b3
raylyanway <raylyanway@gmail.com>
2018-10-07 18:16:51 +0300

    refactor(progression.js)

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,18 +5,18 @@ const progressionLength = 10;
 const getProgression = () => {
   const firstElementPosition = getRandomNumber(0, 10);
   const step = getRandomNumber(1, 10);
-  const hiddenElementPosition = getRandomNumber(0, 10);
   const progression = [];
   for (let i = 0; i < progressionLength; i += 1) {
     progression.push(firstElementPosition + step * i);
   }
-  const rightAnswer = progression[hiddenElementPosition];
-  progression[hiddenElementPosition] = '..';
-  return [progression, rightAnswer];
+  return progression;
 };
 const description = 'What number is missing in this progression?';
 const getQuestion = () => {
-  const [progression, rightAnswer] = getProgression();
+  const progression = getProgression();
+  const hiddenElementPosition = getRandomNumber(0, 10);
+  const rightAnswer = progression[hiddenElementPosition];
+  progression[hiddenElementPosition] = '..';
   const question = progression.join(' ');
   return [String(rightAnswer), question];
 };
